38c772a2c072683726cfb5e29916f9dad354f777
Juan C. Poos <94860562+JuanCPoos@users.noreply.github.com>
2023-01-11 20:09:32 -0300

    Capitulo 8 - Inicio

---
+++ Proyecto Cap 8 Ej 1.1/Proyecto Cap 8 Ej 1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Cap_8_Ej_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, año;
+
+            Console.WriteLine("Ingrese día");
+            dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese mes");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese año");
+            año = int.Parse(Console.ReadLine());
+
+            if(dia == 25 && mes == 12)
+            {
+                Console.WriteLine("Es navidad");
+            }
+            else
+            {
+                Console.WriteLine("No es navidad");
+            }
+            Console.ReadKey(); 
+
+        }
+    }
+}
---
+++ Proyecto Cap 8 Ej 1/Proyecto Cap 8 Ej 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Cap_8_Ej_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int día, mes, año;
+            string linea;
+
+            Console.WriteLine("Ingrese el día:");
+            día = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el mes:");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el año:");
+            año = int.Parse(Console.ReadLine());
+            
+            if(año == DateTime.Now.Year)
+            {
+                if (mes == 1 || mes == 2 || mes == 3)
+                {
+                    Console.WriteLine("La fecha ingresada corresponde al primer trimestre del año");
+                }
+                else
+                {
+                    Console.WriteLine("La fecha ingresa no corresponde al primer trimestre del año")
+                }
+            }
+            else
+            {
+                Console.WriteLine("Año no correspondiente");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
